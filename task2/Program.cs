478de211c9f8cddbe40ad8ad38f2729324774baa
Aleksandra <kharchenko.aleksandra@yandex.ru>
2022-09-28 23:19:37 +0300

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+int m = 1, n = 15;
+System.Console.WriteLine(Real(m, n));
+int Real(int m, int n)
+{
+    int a = -1;
+    if (m > n) a = 1;
+
+    if (n == m)
+        return n;
+    return Real(m, n + a) + n;
+}
